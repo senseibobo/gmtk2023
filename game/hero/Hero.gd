3df47f9396c6e6abb4a0393cced26fd3c3093c65
Bogdan <81476458+senseibobo@users.noreply.github.com>
2023-07-09 14:38:35 +0200

    started work on sounds

--- game/hero/Hero.gd
+++ game/hero/Hero.gd
@@ -3,10 +3,26 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -460.0
+const wood_sounds = [
+	preload("res://sound/footsteps wood/footsteps wood 1.wav"),
+	preload("res://sound/footsteps wood/footsteps wood 2.wav"),
+	preload("res://sound/footsteps wood/footsteps wood 3.wav"),
+	preload("res://sound/footsteps wood/footsteps wood 4.wav"),
+	preload("res://sound/footsteps wood/footsteps wood 5.wav")]
 var gravity = 900
+var timer: Timer = Timer.new()
 
 func _ready():
+	add_child(timer)
+	timer.start(0.2)
+	timer.timeout.connect(footstep)
 	Global.hero = self
+
+func footstep():
+	if $RayCast2D.is_colliding():
+		var collider = $RayCast2D.get_collider()
+		if collider.is_in_group("Wood"):
+			Global.play_sound(wood_sounds[randi()%wood_sounds.size()])
 
 func _physics_process(delta):
 	if not is_on_floor():
--- global/Global.gd
+++ global/Global.gd
@@ -14,6 +14,18 @@ func _ready():
 	if FileAccess.file_exists("user://save.json"):
 		load_game()
 
+func play_sound(stream: AudioStream, from_position: float = 0.0, volume_db: float = 0.0, pitch: float = 0.0, bus: String = "Master"):
+	var player = AudioStreamPlayer.new()
+	player.stream = stream
+	player.volume_db = volume_db
+	player.pitch_scale = pitch
+	player.bus = bus
+	add_child(player)
+	player.play()
+	player.finished.connect(player.queue_free)
+	return player
+	
+
 func reset_data():
 	var file = DirAccess.open("user://")
 	file.remove("save.json")
